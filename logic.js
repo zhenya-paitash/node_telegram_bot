c9915764d24dbae68261e752f03fb853c5ca8ed0
zhenya-paitash <naitavv93@gmail.com>
2021-05-17 19:04:34 +0300

    fix game logic

--- logic.js
+++ logic.js
@@ -25,6 +25,17 @@ const gameBtn = {
   }),
 }
 
+const againBtn = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [[{ text: 'Play again?', callback_data: '/again' }]],
+  }),
+}
+
+const startGame = async (bot, chatId) => {
+  chats[chatId] = Math.floor(Math.random() * 10)
+  await bot.sendMessage(chatId, 'Угадай число от 0 до 9', gameBtn)
+}
+
 export default function (bot) {
   // on MESSAGE
   bot.on('message', async ({ text, chat }) => {
@@ -40,9 +51,7 @@ export default function (bot) {
       case HELP:
         return await bot.sendMessage(chatId, 'this is help')
       case GAME:
-        await bot.sendMessage(chatId, 'Угадай число от 0 до 9')
-        chats[chatId] = Math.floor(Math.random() * 10)
-        return await bot.sendMessage(chatId, 'Отгадай', gameBtn)
+        return startGame(bot, chatId)
       default:
         return await bot.sendMessage(chatId, 'Check my commands pls')
     }
@@ -52,15 +61,19 @@ export default function (bot) {
   bot.on('callback_query', async ({ data, message }) => {
     const chatId = message.chat.id
 
-    if (data === chats[chatId])
-      return bot.sendMessage(
+    if (data === '/again') return startGame(bot, chatId)
+
+    if (+data === +chats[chatId])
+      return await bot.sendMessage(
         chatId,
-        `Поздравляю! Ты отгадал, это было число ${data}`
+        `Поздравляю! Ты отгадал, это было число ${chats[chatId]}`,
+        againBtn
       )
 
-    return bot.sendMessage(
+    return await bot.sendMessage(
       chatId,
-      `К сожалению ты не отгадал. Это было число ${data}`
+      `К сожалению ты не отгадал. Это было число ${chats[chatId]}`,
+      againBtn
     )
   })
 }
